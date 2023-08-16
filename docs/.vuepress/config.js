286326913ede5c75c47765a95e0b83992cbcbe22
byYanXX <by_yanxx@163.com>
2023-08-16 23:00:11 +0800

    first

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,7 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: 'yby\'s blog',
+  description: 'Hello world!',
+})
---
+++ docs/README.md
@@ -0,0 +1,11 @@
+---
+lang: zh-CN
+title: 页面的标题
+description: 页面的描述
+---
+# Hello VuePress
+## 标题二
+### 标题三
+```java
+public class Main {}
+```
